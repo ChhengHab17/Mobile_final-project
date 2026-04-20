6e30d7dac78773e0d5abdb849228a40184f44ad1
Chanrat Sophal <chanrothsophal@gmail.com>
2026-04-20 22:39:52 +0700

    PVT-18 Booking global state

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -5,6 +5,7 @@ import 'package:final_project/data/repositories/station/station_repository_fireb
 import 'package:final_project/data/repositories/subscription_plan/subscription_plan_firebase.dart';
 import 'package:final_project/data/repositories/subscription_plan/subscription_plan_repository.dart';
 import 'package:final_project/main_common.dart';
+import 'package:final_project/ui/state/booking_state.dart';
 import 'package:final_project/ui/state/subscription_state.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +13,7 @@ import 'package:provider/provider.dart';
 List<InheritedProvider> get devProviders{
   return [
     ChangeNotifierProvider<UserPassState>(create: (_) => UserPassState()),
+    ChangeNotifierProvider<BookingState>(create: (_) => BookingState()),
     Provider<StationRepository>(create: (_) => StationRepositoryFirebase()),
     Provider<BikeRepository>(create: (_) => BikeRepositoryFirebase()),
     Provider<SubscriptionRepository>(
---
+++ lib/ui/state/booking_state.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BookingState extends ChangeNotifier {
+	String? _bookedBikeId;
+	String? _bookedStationId;
+
+	String? get bookedBikeId => _bookedBikeId;
+	String? get bookedStationId => _bookedStationId;
+
+	void bookBike(String bikeId, String stationId) {
+		_bookedBikeId = bikeId;
+		_bookedStationId = stationId;
+		notifyListeners();
+	}
+
+	void clearBookedBike() {
+		_bookedBikeId = null;
+		_bookedStationId = null;
+		notifyListeners();
+	}
+}
